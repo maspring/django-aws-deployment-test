819437afb8ba41eb324a1f6697325a58d3384956
Mark Springfield <maspring@aol.com>
2015-05-28 11:47:59 -0700

    added .ebextensions/02_python.config

--- djangoshell/settings.py
+++ djangoshell/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = 'djangoshell.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'awstest',
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
 }
 
 
